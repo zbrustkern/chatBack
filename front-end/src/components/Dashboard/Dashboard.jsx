2fac6af5e07e712f07c12b86cf5ab9909daab3ca
User Name <zbrustkern@gmail.com>
2024-06-13 11:23:42 -0700

    firebase initialization changes

--- front-end/src/components/Dashboard/Dashboard.jsx
+++ front-end/src/components/Dashboard/Dashboard.jsx
@@ -7,8 +7,7 @@ const Dashboard = ({}) => {
     <main>
       <h1>Welcome, {user.username}</h1>
       <p>
-        This is the dashboard page where you, and only you, can see a dashboard
-        of all of your things.
+        Here we ensure that the components are loading.
       </p>
     </main>
   );
